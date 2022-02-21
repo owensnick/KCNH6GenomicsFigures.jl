a8234a4367863dffcdb37c2847a34ac3f2755aaa
Nick Owens <owensnick@gmail.com>
2022-02-21 15:44:42 +0000

    added plotgene

--- src/plots.jl
+++ src/plots.jl
@@ -38,3 +38,101 @@ function plotmodel(model, data, pred_field, label; pred_trans=x->log10(x + 1), p
     
 
 end
+
+
+
+############## plot gene 
+
+findgene(gene, table::DataFrame; field=1) = findgene(gene, table[!, field])
+function findgene(gene, genes)
+    ind = findall(occursin.(gene, genes))
+    isempty(ind) && error("$gene not found")
+    (length(ind) > 1) && println("Multiple matches for $gene:\n$(genes[ind])")
+    first(ind)
+end
+
+plotgene(args...; kwargs...) = (plot(); plotgene!(args...; kwargs...))
+function plotgene!(gene, meta, tpm, altids=[] ; kwargs...)
+    if isempty(altids)
+        plotgene!(findgene(gene, tpm), meta, tpm; kwargs...)
+    else
+        # ind = findene(gene, altids)
+        ind = findall(occursin.(gene, altids.AltID))
+
+        if isempty(ind)
+            plotgene!(findgene(gene, tpm), meta, tpm; kwargs...)
+        else
+            (length(ind) > 1) && println("Multiple matches for $gene:\n$(genes[ind])")
+            plotgene!(findgene(altids.GeneID[first(ind)], tpm), meta, tpm; suptitle=string(gene, "|"), kwargs...)
+        end
+    end
+end
+function plotgene!(gene::Int, meta, tpm ; suptitle="", samples=["U", "H"], cc=[:steelblue, :orange], kwargs...)
+
+    x = [tpm[gene, Symbol(i)] for i in meta.Label]
+
+    for (c, s) in zip(cc, samples)
+        ind = startswith.(meta.Treat, uppercase(s)) .| startswith.(meta.Treat, lowercase(s))
+        plotgeneset!(meta.Time, x, ind, c, s; kwargs...)
+    end
+    plot!(title=string(suptitle, tpm[gene, 1]) ; kwargs...)
+    ylims!(0, last(ylims()))
+end
+function plotgeneset!(xp, x, ind, c=:auto, l="" ; kwargs...)
+    if sum(ind) > 13
+        plot!(xp[ind], x[ind], c=c, lab=l ; kwargs...)
+    else
+        plot!(xp[ind], x[ind], marker=:auto, c=c, lab=l ; kwargs...)
+    end
+end
+
+#### tick marks time to stage
+
+function stageticks_long()
+    data = [(1, 8), (2, 8.5), (4, 9), (6.5, 10), (8, 10.5), (10, 11), (12.5, 12)]
+    Float64.(first.(data)), string.(last.(data))
+end
+
+
+function stageticks_long_double()
+    data = [(1, 8), (4, 9), (6.5, 10), (10, 11), (12.5, 12),
+            (14, 8), (17, 9), (6.5+13, 10), (10+13, 11), (12.5+13, 12)]
+    Float64.(first.(data)), string.(last.(data))
+end
+
+function stageticks_short()
+    data = [(1, 8), (4, 9), (6.5, 10), (10, 11), (12.5, 12)]
+    Float64.(first.(data)), string.(last.(data))
+end
+
+function stageticks_short_double()
+    data = [(1, 8), (10, 11), (14, 8), (23, 11)]
+    Float64.(first.(data)), string.(last.(data))
+end
+
+
+### QC plots
+
+function pairwisecorrelationplot(meta, tpm ; kwargs...)  
+    M = Matrix(tpm[!, meta.Label])
+    heatmap(corspearman(M) ; kwargs...)
+    vline!([13.5], c=:black, lab="")
+    hline!([13.5], c=:black, lab="")
+    
+    xticks!(stageticks_short_double()...)
+    yticks!(stageticks_short_double()...)
+end
+
+
+function pcaplot(meta, tpm ; kwargs...)
+    
+    M = log10.(Matrix(tpm[!, meta.Label]) .+ 1/3)
+    f = fit(PCA, M)
+    TD = MultivariateStats.transform(f, M)'
+    scatter(TD[:, 1], TD[:, 2], group=meta.Treat, marker=(stroke(0), :auto), zcolor=meta.Time, c=cgrad(:viridis, rev=false) ; kwargs...)
+    ofs = ifelse.(meta.Treat .== "UIC", -1, 1)*4
+    explained = principalvars(f)/tvar(f)
+    xlabel!(string("PC 1: ", round(explained[1], digits=2)))
+    ylabel!(string("PC 2: ", round(explained[2], digits=2)))
+    annotate!([(TD[i, 1] + ofs[i], TD[i, 2], text(meta.Time[i], font(10, "helvetica", ifelse(ofs[i] > 0, :black, :red)))) for i = 1:size(TD, 1)])
+end
